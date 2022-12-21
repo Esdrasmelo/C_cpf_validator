e9faa5b01b26c62c9b6abfb946c35e6d265f17a4
Esdrasmelo <emelomotta@gmail.com>
2022-12-21 12:30:05 -0300

    refactor: change data types

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ void moveNumbersToAnotherArray(char *firstArray, char *secondArray);
 int doesItContainsSpecialChar(char *firstArray);
 int calculateFirstRealVerifyingDigit(char *cpfArray);
 int calculateSecondRealVeryfingDigit(char *cpfArray);
-int areDigitsValids(int fRealVerifyingDigit, int sRealVerifyingDigit, char firstInputDigit, char secondInputDigit);
+int areDigitsValids(unsigned short int fRealVerifyingDigit, unsigned short int sRealVerifyingDigit, unsigned char firstInputDigit, unsigned char secondInputDigit);
 
 int main(void) 
 {
@@ -14,15 +14,15 @@ int main(void)
     printf("Por favor, digite o CPF que será validado: ");
     fgets(cpfInput, 15, stdin);
 
-    int doesCpfContainsSpecialChars = doesItContainsSpecialChar(cpfInput);
-    int firstRealVerifyingDigit = 0, secondRealVerifyingDigit = 0;
+    unsigned short int doesCpfContainsSpecialChars = doesItContainsSpecialChar(cpfInput);
+    unsigned short int firstRealVerifyingDigit = 0, secondRealVerifyingDigit = 0;
 
     if (doesItContainsSpecialChar)
     {
         moveNumbersToAnotherArray(cpfInput, cpfNumbers);
         firstRealVerifyingDigit = calculateFirstRealVerifyingDigit(cpfNumbers);
         secondRealVerifyingDigit = calculateSecondRealVeryfingDigit(cpfNumbers);
-        int areValids = areDigitsValids(firstRealVerifyingDigit, secondRealVerifyingDigit, cpfNumbers[9], cpfNumbers[10]);
+        unsigned short int areValids = areDigitsValids(firstRealVerifyingDigit, secondRealVerifyingDigit, cpfNumbers[9], cpfNumbers[10]);
 
         if (!areValids) printf("\nO CPF digitado é inválido!.\n");
         else printf("\nO CPF digitado é válido!.\n");
@@ -30,7 +30,7 @@ int main(void)
     else {
         firstRealVerifyingDigit = calculateFirstRealVerifyingDigit(cpfInput);
         secondRealVerifyingDigit = calculateSecondRealVeryfingDigit(cpfInput);
-        int areValids = areDigitsValids(firstRealVerifyingDigit, secondRealVerifyingDigit, cpfInput[10], cpfInput[11]);
+        unsigned short int areValids = areDigitsValids(firstRealVerifyingDigit, secondRealVerifyingDigit, cpfInput[10], cpfInput[11]);
 
         if (!areValids) printf("\nO CPF digitado é inválido!.\n");
         else printf("\nO CPF digitado é válido!.\n");
@@ -39,9 +39,9 @@ int main(void)
 
 void moveNumbersToAnotherArray(char *firstArray, char *secondArray)
 {
-    int secArrayCounter = 0;
+    unsigned short int secArrayCounter = 0;
 
-    for (int counter = 0; counter < strlen(firstArray); counter++)
+    for (unsigned short int counter = 0; counter < strlen(firstArray); counter++)
     {
         if (firstArray[counter] != '.' && firstArray[counter] != '-')
         {
@@ -53,9 +53,9 @@ void moveNumbersToAnotherArray(char *firstArray, char *secondArray)
 
 int doesItContainsSpecialChar(char *firstArray)
 {
-    int isItSpecialChar = 0;
+    unsigned int isItSpecialChar = 0;
 
-    for (int counter = 0; counter < strlen(firstArray); counter++)
+    for (unsigned int counter = 0; counter < strlen(firstArray); counter++)
     {
         if (firstArray[counter] == '.' || firstArray[counter] == '-')
             isItSpecialChar = 1;
@@ -66,39 +66,39 @@ int doesItContainsSpecialChar(char *firstArray)
 
 int calculateFirstRealVerifyingDigit(char *cpfArray)
 {
-    int digitsSum = 0;
-    int auxCounter = 10;
+    unsigned short int digitsSum = 0;
+    unsigned short int auxCounter = 10;
 
-    for (int counter = 0; counter < 9; counter++)
+    for (unsigned short int counter = 0; counter < 9; counter++)
     {
         digitsSum += (cpfArray[counter] - '0') * auxCounter;
         auxCounter -= 1;
     }
 
-    int rest = digitsSum % 11;
-    int realVerifyingDigit = rest < 2 ? 0 : 11 - rest;
+    unsigned short int rest = digitsSum % 11;
+    unsigned short int realVerifyingDigit = rest < 2 ? 0 : 11 - rest;
 
     return realVerifyingDigit;
 }
 
 int calculateSecondRealVeryfingDigit(char *cpfArray)
 {
-    int digitsSum = 0;
-    int auxCounter = 11;
+    unsigned short int digitsSum = 0;
+    unsigned short int auxCounter = 11;
 
-    for (int counter = 0; counter < 10; counter++)
+    for (unsigned short int counter = 0; counter < 10; counter++)
     {
         digitsSum += (cpfArray[counter] - '0') * auxCounter;
         auxCounter -= 1;
     }
 
-    int rest = digitsSum % 11;
-    int realVerifyingDigit = rest < 2 ? 0 : 11 - rest;
+    unsigned short int rest = digitsSum % 11;
+    unsigned short int realVerifyingDigit = rest < 2 ? 0 : 11 - rest;
 }
 
-int areDigitsValids(int fRealVerifyingDigit, int sRealVerifyingDigit, char firstInputDigit, char secondInputDigit)
+int areDigitsValids(unsigned short int fRealVerifyingDigit, unsigned short int sRealVerifyingDigit, unsigned char firstInputDigit, unsigned char secondInputDigit)
 {
-    int areValids = 0;
+    unsigned short int areValids = 0;
 
     if (fRealVerifyingDigit == firstInputDigit - '0' && sRealVerifyingDigit == secondInputDigit - '0')
         areValids = 1;
